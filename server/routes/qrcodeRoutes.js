db5328cd475ee7d9554370dd7e64900ed58c34fe
Mogbo Akeredolu <2001mogbo@gmail.com>
2024-12-11 02:05:30 +0100

    deleted commit

--- server/routes/qrcodeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getMenuItems, addMenuItem, deleteMenuItem, generateQRCode } = require('../controllers/qrcodeController');
-
-const router = express.Router();
-
-router.get('/qrcode', generateQRCode); // QR Code route
-
-module.exports = router;
